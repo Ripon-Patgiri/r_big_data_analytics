4ac80b317906230e211fbf172344b81b9d497284
Ripon-Patgiri <riponmongzeell@gmail.com>
2023-06-18 15:48:03 +0530

    Exe 8 : Linear Regression

--- ex_08/linear_regression.R
+++ ex_08/linear_regression.R
@@ -42,3 +42,48 @@ income.graph
 income.graph + theme_bw() + labs(title = "Reported happiness as a function of income",
                                  x = "Income (x$10,000)",
                                  y = "Happiness score (0 to 10)")
+
+
+# ------------------- 
+
+# Install and load the necessary package
+install.packages("MASS")  # Install only if not already installed
+library(MASS)
+
+# Load the dataset
+data(Boston)
+
+# Explore the dataset
+summary(Boston)
+
+# Perform linear regression
+lm_model <- lm(medv ~ ., data = Boston)
+
+# Print the summary of the linear regression model
+summary(lm_model)
+
+# Example: Predicting the price of a house
+
+# Provide the values for the independent variables (features) in the model equation
+new_data <- data.frame(
+  crim = 0.1,    # per capita crime rate by town
+  zn = 20,       # proportion of residential land zoned for lots over 25,000 sq.ft.
+  indus = 8,     # proportion of non-retail business acres per town
+  chas = 0,      # Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+  nox = 0.5,     # nitric oxides concentration (parts per 10 million)
+  rm = 6,        # average number of rooms per dwelling
+  age = 60,      # proportion of owner-occupied units built prior to 1940
+  dis = 5,       # weighted distances to five Boston employment centers
+  rad = 4,       # index of accessibility to radial highways
+  tax = 300,     # full-value property-tax rate per $10,000
+  ptratio = 15,  # pupil-teacher ratio by town
+  black = 350,   # 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+  lstat = 10     # % lower status of the population
+)
+
+# Make predictions using the trained linear regression model
+predicted_prices <- predict(lm_model, newdata = new_data)
+
+# Print the predicted prices
+print(predicted_prices)
+
