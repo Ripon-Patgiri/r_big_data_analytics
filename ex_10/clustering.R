66430ff3327c44cbc060a44658b7a54499df9a1e
Ripon-Patgiri <riponmongzeell@gmail.com>
2023-06-18 16:07:41 +0530

    Exe 10 : Clustering

--- ex_10/clustering.R
+++ ex_10/clustering.R
@@ -62,3 +62,72 @@ plot(hclust_result)
 
 # cut the tree into 3 clusters
 cutree(hclust_result, k = 3)
+
+# ----------------------
+
+
+# Load the dataset
+data("iris")
+
+# Explore the dataset
+summary(iris)
+
+# Perform k-means clustering
+set.seed(123)  # For reproducibility
+kmeans_result <- kmeans(iris[, -5], centers = 3)  # Clustering using the first 4 columns (features)
+
+# Add cluster labels to the dataset
+iris$cluster_kmeans <- as.factor(kmeans_result$cluster)
+
+# Perform hierarchical clustering
+hclust_result <- hclust(dist(iris[, -5]))  # Clustering using the first 4 columns (features)
+
+# Cut the dendrogram to get a specific number of clusters
+cut_tree <- cutree(hclust_result, k = 3)
+
+# Add cluster labels to the dataset
+iris$cluster_hierarchical <- as.factor(cut_tree)
+# Visualize the clusters (k-means)
+library(ggplot2)
+ggplot(iris, aes(Petal.Length, Petal.Width, color = cluster_kmeans)) +
+  geom_point() +
+  labs(title = "k-Means Clustering")
+
+# Visualize the clusters (hierarchical)
+ggplot(iris, aes(Petal.Length, Petal.Width, color = cluster_hierarchical)) +
+  geom_point() +
+  labs(title = "Hierarchical Clustering")
+
+# Load the dataset
+data("USArrests")
+
+# Explore the dataset
+summary(USArrests)
+
+# Perform k-means clustering
+set.seed(123)  # For reproducibility
+kmeans_result <- kmeans(USArrests, centers = 3)
+
+# Add cluster labels to the dataset
+USArrests$cluster_kmeans <- as.factor(kmeans_result$cluster)
+
+# Perform hierarchical clustering
+hclust_result <- hclust(dist(USArrests))
+
+# Cut the dendrogram to get a specific number of clusters
+cut_tree <- cutree(hclust_result, k = 3)
+
+# Add cluster labels to the dataset
+USArrests$cluster_hierarchical <- as.factor(cut_tree)
+
+# Visualize the clusters (k-means)
+library(ggplot2)
+ggplot(USArrests, aes(UrbanPop, Murder, color = cluster_kmeans)) +
+  geom_point() +
+  labs(title = "k-Means Clustering")
+
+# Visualize the clusters (hierarchical)
+ggplot(USArrests, aes(UrbanPop, Murder, color = cluster_hierarchical)) +
+  geom_point() +
+  labs(title = "Hierarchical Clustering")
+
